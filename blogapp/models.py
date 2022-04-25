7e65297dba2700bc8c047e78291c43431c1ba861
Emmanuel-Okyere <57065066+Emmanuel-Okyere@users.noreply.github.com>
2022-04-25 16:49:57 +0000

    Finished Chapter 1

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 # Create your models here.
+
+#Creating custom models
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status="published")
+
+
+
 class Post(models.Model):
-    STATUS_CHOICES = (("draft, :Draft"), ("published","Published"),)
+    STATUS_CHOICES = (("draft", "Draft"), ("published","Published"),)
     title = models.CharField(max_length= 250)
     slug = models.SlugField(max_length=250, unique_for_date="publish")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
@@ -11,5 +20,18 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add= True)
     updated = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
+    status = models.CharField(max_length=10, choices= STATUS_CHOICES, default="draft")
+
+    class Meta:
+        ordering = ("-publish", )
     
+    def __str__(self):
+        return self.title
+    
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse("blogapp:post_detail", args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+    
+
