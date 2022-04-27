04293a47f896a699dec34ee30de78341adc7b7e7
Emmanuel-Okyere <57065066+Emmanuel-Okyere@users.noreply.github.com>
2022-04-27 12:56:42 +0000

    Changed Access

--- blog/settings.py
+++ blog/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 # from curses.ascii import EM
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u1*3ke+_x0f5c-cb6dkr6$4gr5^f6o21fv@qisq)f1#j%5+16h'
-
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -127,8 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Adding SMTP
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "gyateng94@gmail.com"
-EMAIL_HOST_PASSWORD = "okyere123"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
