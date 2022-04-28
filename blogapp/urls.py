2d9acf5e1f76ef83550539c8ff5905e2304cc831
Emmanuel-Okyere <57065066+Emmanuel-Okyere@users.noreply.github.com>
2022-04-28 14:32:37 +0000

    Fixed the pagination error

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from django.contrib.sitemaps.views import sitemap
 from blogapp import views
 from blogapp.sitemaps import PostSitemap
+from .feeds import LatestPostsFeed
 app_name = "blogapp"
 
 sitemaps = {
@@ -14,5 +15,7 @@ urlpatterns = [
     path("<int:year>/<int:month>/<int:day>/<slug:post>/", views.post_detail, name ="post_detail"),
     path("<int:post_id>/share", views.post_share, name= "post_share"),
     path("tag/<slug:tag_slug>/",views.post_list,name = "post_list_by_tag"),
-    path("sitemap.xml", sitemap, {"sitemaps":sitemaps},name='django.contrib.sitemaps.views.sitemap')
+    path("sitemap.xml", sitemap, {"sitemaps":sitemaps},
+    name='django.contrib.sitemaps.views.sitemap'),
+    path("feed/",LatestPostsFeed(), name = "post_feed")
 ]
